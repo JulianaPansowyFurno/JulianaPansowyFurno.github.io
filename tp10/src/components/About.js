8dbe8540793778a3ae7adc901d3df9ef696f37a6
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-30 16:02:56 -0300

    primera clase

---
+++ tp10/src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+  return (
+    <h1> Hola</h1>
+  );
+}
+
+export default About;
---
+++ tp10/src/components/global/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <h1> Hola</h1>
+  );
+}
+
+export default Footer;
