dd3bb4f7d3c61f8afa121c49b99c61208c3b9c1f
JulianaPansowyFurno <julipansofurno@gmail.com>
2023-11-21 20:17:40 -0300

    terminar home 2 y favoritos

--- tp10/src/components/Detalle.js
+++ tp10/src/components/Detalle.js
@@ -1,15 +1,19 @@
 import React, {useState, useEffect} from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Particle from "./Particle";
 import {useProductsData} from "../components/MyContext"
 import axios from 'axios';
 import { useParams } from "react-router-dom";
+import Particle from "../components/Particle";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { CgWebsite } from "react-icons/cg";
+import { CiSaveUp1 } from "react-icons/ci";
+import { IoIosLink } from "react-icons/io";
 
 function Detalle() {
     const {id} = useParams();
-    const { data } = useProductsData([]);
-    const [proyecto, setProyecto] = useState(null);
-    const [count, setCount] = useState(0);
+    const { data } = useProductsData();
+    const [proyecto, setProyecto] = useState([]);
 
     useEffect(() => {
       data.filter(d => 
@@ -20,18 +24,56 @@ function Detalle() {
         }
       )      
     }, [data])
-    console.log(data[0].id == id)
+    
     console.log(proyecto)
-  return (
-    <>
-    <p>{proyecto.id}</p>   
-    {/* <Container fluid className="project-section">
-      <Particle />
-      <Container> 
-        
-      </Container>
-    </Container> */}
-    </>
+  return (  
+    <Container fluid className="about-section">
+    <Particle />
+    <Container>
+      <Row style={{ justifyContent: "center", padding: "10px" }}>
+        <Col
+          md={7}
+          style={{
+            justifyContent: "center",
+            paddingTop: "30px",
+            paddingBottom: "50px",
+          }}
+        >
+          <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+            El titulo de esta creacion es<strong className="purple"> {proyecto.titulo}</strong>
+          </h1>
+              <Card className="quote-card-view">
+          <Card.Body>
+            <blockquote className="blockquote mb-0">
+              <p style={{ textAlign: "justify" }}>
+              Esta creacion es sobre <span className="purple"> {proyecto.descripcion} </span>
+              <br /> 
+               Se realizo el  <span className="purple"> {proyecto.fecha}</span>
+                <br />
+                <br />
+                Boton hacia la url del proyecto:
+              </p>
+              <Button variant="primary" href={proyecto.url} target="_blank">
+              <IoIosLink /> &nbsp;
+          URL
+        </Button>
+
+            </blockquote>
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col
+          md={5}
+          style={{ paddingTop: "120px", paddingBottom: "50px" }}
+          className="about-img"
+        >
+        </Col>
+      </Row>
+
+  
+    </Container>
+  </Container>
+    
   );
 }
 
--- tp10/src/components/Home/Home2.js
+++ tp10/src/components/Home/Home2.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React, {useState, useEffect}  from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Tilt from "react-parallax-tilt";
 import {
@@ -10,22 +10,70 @@ import { FaLinkedinIn } from "react-icons/fa";
 import axios from 'axios';
 import ProjectCard from "../Projects/ProjectCards";
 import { useNavigate } from "react-router-dom";
+import {useProductsData} from "../MyContext";
 
 function Home2() {
   const navigate = useNavigate();
-   
+  const { data } = useProductsData();
+  const [proyecto, setProyecto] = useState([]);
   
+  console.log(data)
+  useEffect(() => {
+    if(data)
+    {setProyecto(data)}
+    else{alert("no cargo")}
+  }, [])
+
+  useEffect(() => {
+    if(data)
+    {setProyecto(data)}
+    else{alert("no cargo")}
+  }, [data])
+  console.log(data)
   
   return (
+  
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
-          
-        
-          <Col md={4} className="project-card">
+          {/* VER COMO SOLUCUIONAR EL MOSTAR ESTA CARDS /*}
+           {/* <Col md={4} className="project-card">
+          <ProjectCard 
+          key={proyecto[0].id}
+            imgPath={proyecto[0].imagen}
+            title={proyecto[0].titulo}
+            description={proyecto[0].descripcion}
+            ghLink={proyecto[0].url}
+            onClickDetalle={() => navigate(`/detalle/${proyecto[0].id}`)}          
+          />
+        </Col> 
+
+         <Col md={4} className="project-card">
+          <ProjectCard 
+          key={proyecto[1].id}
+            imgPath={proyecto[1].imagen}
+            title={proyecto[1].titulo}
+            description={proyecto[1].descripcion}
+            ghLink={proyecto[1].url}
+            onClickDetalle={() => navigate(`/detalle/${proyecto[1].id}`)}          
+          />
+        </Col>
+
+
+        <Col md={4} className="project-card">
+          <ProjectCard 
+          key={proyecto[2].id}
+            imgPath={proyecto[2].imagen}
+            title={proyecto[2].titulo}
+            description={proyecto[2].descripcion}
+            ghLink={proyecto[2].url}
+            onClickDetalle={() => navigate(`/detalle/${proyecto[1].id}`)}          
+          />
+        </Col>  */}
+
+          {/* <Col md={4} className="project-card">
           <ProjectCard
             imgPath="https://i.pinimg.com/474x/95/7f/c1/957fc1d05370489fe519edcb03d75ebc.jpg"
-            title="Hola"
             description="Muy Bueno"
             ghLink="https://i.pinimg.com/474x/95/7f/c1/957fc1d05370489fe519edcb03d75ebc.jpg"
           />
@@ -46,9 +94,10 @@ function Home2() {
             ghLink="https://i.pinimg.com/474x/95/7f/c1/957fc1d05370489fe519edcb03d75ebc.jpg"
            
           />
-        </Col>
+        </Col> */}
 
         </Row>
+  
         <Row>
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
@@ -99,6 +148,8 @@ function Home2() {
             </ul>
           </Col>
         </Row>
+       
+          
       </Container>
     </Container>
   );
